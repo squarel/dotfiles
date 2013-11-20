68e9a47d05bf8834d784ff0e573a839b27a8fc05
squarel <squarelnix@gmail.com>
2013-11-20 16:33:56 -0500

    add python development plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,79 @@
+;;; package -- Summary
+;;; Commentary:
+
+;;; Code:
+;; Path
+(setq exec-path (append exec-path '("/usr/local/bin")))
 (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/el-get/color-theme-zenburn")
+
+;; Theme
+(load-theme 'zenburn t)
 
 ;; Turn off mouse interface
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-linum-mode 1)
-(ido-mode 1)
+
+;; Ido mode
+(require 'ido)
+(ido-mode t)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
+
+;; Inhibit startup message
+(setq inhibit-startup-message t)
+
+;; Fullscreen
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+(toggle-fullscreen)
+
+;; Package
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; El-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+(el-get 'sync)
+
+;; Jedi
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+
+;; Flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Autopair
+(require 'autopair)
+(autopair-global-mode) ;; to enable in all buffers
+
 
 (custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
-
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-gnome2)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;; init.el ends here
