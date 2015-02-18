ebfb6324309b4d98a1b343fa11fb74400d3517b9
Xiaolu Li <xli@kyru.us>
2015-02-18 16:40:31 -0500

    add python environment module

---
+++ python/install.sh
@@ -0,0 +1,6 @@
+brew update
+brew install python --build-from-source
+pip install --upgrade pip
+pip install virtualenv
+pip install virtualenvwrapper
+sudo rm -f /usr/bin/python # remember to delete it every time
